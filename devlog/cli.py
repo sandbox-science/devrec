3964a34092e9f7c1f42845c721a6d2914c6db847
Chris <rolletchris1@gmail.com>
2025-07-28 19:52:39 -0700

    Added export function + doctstring/mypy

--- devlog/cli.py
+++ devlog/cli.py
@@ -1,5 +1,6 @@
 import typer
 
+from datetime import datetime
 from pathlib import Path
 from .session import Session
 from .logger import Logger
@@ -11,14 +12,23 @@ CURRENT = DATA_DIR.parent / "current.json"
 
 
 @app.command()
-def start():
+def start() -> None:
+    """
+    Start a session.
+    """
     session = Session.start_new(DATA_DIR)
     CURRENT.write_text(str(session.json_path))
     typer.echo(f"✅ Session started: {session.id}")
 
 
 @app.command()
-def note(message: str):
+def note(message: str) -> None:
+    """
+    Add a note to the active session.
+
+    :param message: The note the user want to log.
+    :type message: str
+    """
     path = Path(CURRENT.read_text())
     session = Session.load(path)
     Logger(session).note(message)
@@ -26,14 +36,33 @@ def note(message: str):
 
 
 @app.command()
-def stop():
+def stop() -> None:
+    """
+    Stop an active session.
+    """
     path = Path(CURRENT.read_text())
     session = Session.load(path)
     session.stop()
     session.save()
-    session.export_markdown()
     CURRENT.unlink()
-    typer.echo(f"✅ Session saved to {session.md_path}")
+    typer.echo("✅ Session ended.")
+
+
+@app.command()
+def export(format: str) -> None:
+    """
+    Export logs to markdwon, html, etc.
+
+    :param format: The format the user want to export the logs.
+    :type format: str
+    """
+    # TODO: Find a better way to export files
+    today_file = DATA_DIR / f"{datetime.now().strftime('%Y-%m-%d')}.json"
+    session = Session.load(Path(today_file))
+    session.save()
+    if format == "md":
+        session.export_markdown()
+        typer.echo(f"🚀 Log exported to {session.md_path}")
 
 
 if __name__ == "__main__":
